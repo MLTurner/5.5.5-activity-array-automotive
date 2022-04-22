444667beb45cd1860eb66164d5e1c0c6a639fdeb
MLTurner <mlturne3@ncsu.edu>
2022-04-21 22:28:17 -0400

    initialized methods

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,18 +1,10 @@
 //this includes the vehicle class as a module
 const VehicleModule = require("./vehicle")
 
-//this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
 
 class NewCar extends VehicleModule {
     constructor(make, model, year, color, mileage) {
-    super(maximumPassengers, passenger, numberOfWheels, maximumPassengers, fuel, scheduleService) 
-        this.make = "Mercury";
-        this.model = "Sedan";
-        this.year = "1965";
-        this.color = "silver";
-        this.mileage = "29000";
+    super(make, model, year, color, mileage); { 
         this.maximumPassengers = "5";
         this.passenger = "0";
         this.numberOfWheels = "4";
@@ -24,16 +16,24 @@ class NewCar extends VehicleModule {
 
 loadPassenger(num) {
     if (this.passenger < this.maximumPassengers) {
-    return availableRoom == false;
+    return availableRoom == "false";
     }
 }
 
 start() {
     if (this.fuel > 0)
-    return start == true;
+    return start == "true";
 }
 
 scheduleService(mileage) {
     if (this.mileage > 30000)
-    return this.scheduleService == true;
+    return this.scheduleService == "true";
 }
+
+let v = new NewCar("Mecury", "Sedan", "1965", "silver", 29000);
+console.log(v.make)
+
+
+v.start()
+v.loadPassenger(5)
+v.scheduleService()
