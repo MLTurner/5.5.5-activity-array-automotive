a1430a4251e1d97d7d2b360021384a5f0b2f1a26
MLTurner <mlturne3@ncsu.edu>
2022-04-21 22:09:12 -0400

    added super, refactored code

--- mecurySedan.js
+++ mecurySedan.js
@@ -5,7 +5,35 @@ const VehicleModule = require("./vehicle")
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
 
-class newCar extends VehicleModule {
-    constructor
+class NewCar extends VehicleModule {
+    constructor(make, model, year, color, mileage) {
+    super(maximumPassengers, passenger, numberOfWheels, maximumPassengers, fuel, scheduleService) 
+        this.make = "Mercury";
+        this.model = "Sedan";
+        this.year = "1965";
+        this.color = "silver";
+        this.mileage = "29000";
+        this.maximumPassengers = "5";
+        this.passenger = "0";
+        this.numberOfWheels = "4";
+        this.maximumSpeed = "160";
+        this.fuel = "10";
+        this.scheduleService = "false"
+    }
+}
 
+loadPassenger(num) {
+    if (this.passenger < this.maximumPassengers) {
+    return availableRoom == false;
+    }
+}
+
+start() {
+    if (this.fuel > 0)
+    return start == true;
+}
+
+scheduleService(mileage) {
+    if (this.mileage > 30000)
+    return this.scheduleService == true;
 }
